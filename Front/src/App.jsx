2e89fcb1d084a6fff290c08c250bb71bc7be517f
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-14 17:51:19 -0300

    agrego login

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./commons/login";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hola</h1>
+      <Login />
     </>
   );
 }
--- Front/src/commons/login.jsx
+++ Front/src/commons/login.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-function login() {}
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default login;
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/users/login",
+        {
+          email: email,
+          password: password,
+        },
+        { withCredential: true }
+      )
+      .then((res) => res.data)
+      .then(() => {
+        setPassword("");
+        setEmail("");
+      });
+  };
+
+  return (
+    <>
+      <h1>Tu nueva vivienda está aqui</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          value={email}
+          onChange={handleChangeEmail}
+          placeholder="EMAIL"
+        ></input>
+        <br />
+
+        <input
+          value={password}
+          onChange={handleChangePassword}
+          type="password"
+          placeholder="PASSWORD"
+        ></input>
+        <br />
+        <p>¿Olvidaste tu contraseña?</p>
+        <button>REGISTER</button>
+        <button type="submit">LOG IN</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
