4d0a32bee2e97bb2edfbda0481b8db27c599190f
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-14 17:37:07 -0300

    imagen

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./components/login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hola</h1>
+      <Login />
     </>
   );
 }
