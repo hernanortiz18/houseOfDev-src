4bba8942e79ab3f021845d2a76b25ba7b16324a8
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2023-11-14 17:34:42 -0300

    add component user Register

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Register from "./commons/Register.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Register />
   </BrowserRouter>
 );
