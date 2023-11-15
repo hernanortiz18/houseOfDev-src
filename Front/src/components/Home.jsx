56e316fd65cb00c4e5e38e76dbb17c4bbbd56776
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-15 18:58:00 -0300

    hola

--- Front/src/components/Home.jsx
+++ Front/src/components/Home.jsx
@@ -6,7 +6,6 @@ import imagenDos from "../assets/img/Group179.png";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function Home() {
   useEffect(() => {
     axios
@@ -14,21 +13,20 @@ function Home() {
       .then((response) => response.data)
       .catch((error) => {
         console.log(error);
+        s;
       });
   }, []);
   console.log("hola");
   return (
-
-    <div className="container-fluid h-100 text-center p-0">
+    <div className="container-fluid h-100 text-center">
       <div className="row h-100 m-0">
-        <div className="col d-flex align-items-center justify-content-center p-0">
+        <div className="col d-flex align-items-center justify-content-center">
           <Link to="/login" style={{ width: "100%", height: "100%" }}>
             <img src={imagen} alt="imagen" className="w-100 h-100" />
           </Link>
         </div>
       </div>
     </div>
-
   );
 }
 
