0ce14c8197544eae700ec14cb09f79cc655d6638
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-15 11:28:49 -0300

    update de rutas

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Login from "./commons/login";
-import Register from "./commons/Register";
+import Login from "./components/login";
+import Register from "./components/Register";
 import { Route, Routes } from "react-router";
+import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
