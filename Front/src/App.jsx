d4c4dbe3b4103a97014e7f4800b594202ad3b26f
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2023-11-15 15:57:31 -0300

    changes in register.scss

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./components/login";
 import Register from "./components/Register";
@@ -9,7 +9,7 @@ import Home from "./components/Home";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
--- Front/src/components/Register.jsx
+++ Front/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import "../styles/register.scss";
+import image from "../assets/img/Group 179.png";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -36,50 +37,60 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="formRegister">
-      <input
-        value={registerData.name}
-        type="text"
-        name="name"
-        placeholder="Name"
-        onChange={handleChange}
-      />
-      <br />
-      <input
-        value={registerData.lastName}
-        type="text"
-        name="lastName"
-        placeholder="Last name"
-        onChange={handleChange}
-      />
-      <br />
-      <input
-        value={registerData.phone}
-        type="text"
-        name="phone"
-        placeholder="Phone"
-        onChange={handleChange}
-      />
-      <br />
-      <input
-        value={registerData.email}
-        type="text"
-        name="email"
-        placeholder="E-mail"
-        onChange={handleChange}
-      />
-      <br />
-      <input
-        value={registerData.password}
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-      />
-      <br />
-      <button type="submit">Register</button>
-      <button>Log in</button>
-    </form>
+    <div className="bg-color">
+      <div className="bg-imagen">
+        <div className="register-container">
+          <div className="hod-logo">
+            <p className="house">HOUSE</p>
+            <p className="ofDev">OF DEV</p>
+          </div>
+          <form onSubmit={handleSubmit} className="formRegister">
+            <input
+              value={registerData.name}
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              value={registerData.lastName}
+              type="text"
+              name="lastName"
+              placeholder="Last name"
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              value={registerData.phone}
+              type="text"
+              name="phone"
+              placeholder="Phone"
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              value={registerData.email}
+              type="text"
+              name="email"
+              placeholder="E-mail"
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              value={registerData.password}
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <br />
+            <button type="submit">Register</button>
+            <button>Log in</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
