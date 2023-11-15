479815284d357d9919bc08c51b239ed6de4c7440
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-15 18:21:02 -0300

    update home

--- Front/src/components/Home.jsx
+++ Front/src/components/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import imagen from "../assets/img/Thumbnail.png";
 import imagenDos from "../assets/img/Group179.png";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
   useEffect(() => {
@@ -15,10 +16,10 @@ function Home() {
   }, []);
 
   return (
-    <div className="container text center">
-      <div className="row">
-        <div className="col">
-          <Link to="/login">
+    <div className="container-fluid h-100 text-center p-0">
+      <div className="row h-100 m-0">
+        <div className="col d-flex align-items-center justify-content-center p-0">
+          <Link to="/login" style={{ width: "100%", height: "100%" }}>
             <img src={imagen} alt="imagen" className="w-100 h-100" />
           </Link>
         </div>
