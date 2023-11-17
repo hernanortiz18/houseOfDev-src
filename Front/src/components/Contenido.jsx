851fbaeb23463fdaa0b98e431537a7b5c923cdae
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-17 13:24:40 -0300

    add contenido

---
+++ Front/src/components/Contenido.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../commons/Navbar";
+
+function Contenido() {
+  return (
+    <>
+      <Navbar />
+      <h2>Contenido</h2>
+    </>
+  );
+}
+
+export default Contenido;
--- Front/src/components/Home.jsx
+++ Front/src/components/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
   return (
     <div className="row h-100 m-0">
       <div className="col d-flex align-items-center justify-content-center">
-        <Link to="/login" style={{ width: "100%", height: "100%" }}>
+        <Link to="/contenido" style={{ width: "100%", height: "100%" }}>
           <img src={imagen} alt="imagen" className="w-100 h-100" />
         </Link>
       </div>
