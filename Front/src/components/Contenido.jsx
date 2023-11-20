8c6d34c8f8b0c720fc97906db38ca551975062df
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-20 17:06:31 -0300

    add userProperty

--- Front/src/components/Contenido.jsx
+++ Front/src/components/Contenido.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../commons/Navbar";
 import "../styles/contenido.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 //import "../styles/navbar.scss";
 function Contenido() {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [ubicacion, setUbicacion] = useState("");
+
+  const handleOptionBusqueda = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleUbicacion = (e) => {
+    setUbicacion(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("/api/search/", { search: search })
+      .then((res) => res.data)
+      .then((search) => setSearchResult(search.results))
+      .catch((erro) => console.log("error"));
+  };
+
   return (
     <div className="container">
       <Navbar />
       <div className="form-container">
         <h2>¿Qué estás buscando?</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-check">
             <input
               className="form-check-input"
@@ -18,6 +41,7 @@ function Contenido() {
               name="opcionBusqueda"
               id="alquiler"
               value="alquiler"
+              onChange={handleOptionBusqueda}
             />
             <label className="form-check-label" htmlFor="alquiler">
               Alquiler
@@ -30,6 +54,7 @@ function Contenido() {
               name="opcionBusqueda"
               id="comprar"
               value="comprar"
+              onChange={handleOptionBusqueda}
             />
             <label className="form-check-label" htmlFor="comprar">
               Comprar
@@ -40,17 +65,39 @@ function Contenido() {
               type="text"
               className="form-control"
               placeholder="🔍 Ubicación"
+              value={ubicacion}
+              onChange={handleUbicacion}
             />
           </div>
-          <Link>
+          <Link to={"/results"}>
             <button type="button" className="btn btn-primary">
               Ver Propiedades
             </button>
           </Link>
         </form>
       </div>
+
+      <div className="container">
+        <div className="row">
+          {searchResult.map((data, i) => (
+            <div className="col-md-4" key={i}>
+              <PropertyCard dataPropertyCard={data} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+
+// <Link to={"/results"}>
+// <button
+//   type="button"
+//   className="btn btn-primary"
+//   onChange={handleUbicacion}
+// >
+//   Ver Propiedades
+// </button>
+// </Link>
 
 export default Contenido;
