143505c1e7cc06704b27c811457a0578553da791
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-22 12:20:11 -0300

    add vistaVenta

---
+++ Front/src/components/Venta.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../commons/Navbar";
+import "../styles/venta.scss";
+
+function Venta() {
+  return (
+    <>
+      <Navbar />
+      <h2>Propiedades en Venta</h2>
+    </>
+  );
+}
+
+export default Venta;
