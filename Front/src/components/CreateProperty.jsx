0663de140291d0915540cdf887a79e5bc9fcd7d5
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-24 18:55:28 -0300

    change createProperty

--- Front/src/components/CreateProperty.jsx
+++ Front/src/components/CreateProperty.jsx
@@ -1,33 +1,196 @@
+import axios from "axios";
 import React, { useState } from "react";
+import NavbarAdmin from "../commons/NavbarAdmin";
 
 function CreateProperty() {
   const [province, setProvince] = useState("");
   const [city, setCity] = useState("");
   const [address, setAddress] = useState("");
   const [number, setNumber] = useState("");
-  const [onSale, setOnsale] = useState(Boolean);
-  const [img, setImg] = useState([]);
+  const [onSale, setOnsale] = useState("");
+  const [price, setPrice] = useState("");
+  const [img, setImg] = useState("");
   const [squareMeters, setSquareMeters] = useState("");
   const [bathrooms, setBathrooms] = useState("");
   const [bedrooms, setBedrooms] = useState("");
 
-  const handleSubmit = (e) => {};
+  const handleProvince = (e) => {
+    setProvince(e.target.value);
+  };
+  const handleCity = (e) => {
+    setCity(e.target.value);
+  };
+  const handleAddress = (e) => {
+    setAddress(e.target.value);
+  };
+  const handleNumber = (e) => {
+    setNumber(e.target.value);
+  };
+  const handleOnsale = (e) => {
+    setOnsale(e.target.value);
+  };
+  const handlePrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleImage = (e) => {
+    setImg(e.target.value);
+  };
+  const handleSquareMeters = (e) => {
+    setSquareMeters(e.target.value);
+  };
+  const handleBathrooms = (e) => {
+    setBathrooms(e.target.value);
+  };
+  const handleBedrooms = (e) => {
+    setBedrooms(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/properties/register",
+        {
+          province: province,
+          city: city,
+          address: address,
+          number: number,
+          onSale: onSale,
+          price: price,
+          img: img,
+          squareMeters: squareMeters,
+          bathrooms: bathrooms,
+          bedrooms: bedrooms,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => res.data)
+      .then(() => {
+        setProvince("");
+        setCity("");
+        setAddress("");
+        setNumber("");
+        setOnsale("");
+        setImg("");
+        setSquareMeters("");
+        setBathrooms("");
+        setBedrooms("");
+      });
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <h1>Nueva Propiedad</h1>
-        <input placeholder="province" className="form-control"></input>
-        <input placeholder="city" className="form-control"></input>
-        <input placeholder="address" className="form-control"></input>
-        <input placeholder="number" className="form-control"></input>
-        <input placeholder="onSale" className="form-control"></input>
-        <input placeholder="img" className="form-control"></input>
-        <input placeholder="squareMeters" className="form-control"></input>
-        <input placeholder="bathrooms" className="form-control"></input>
-        <input placeholder="bedrooms" className="form-control"></input>
-        <br />
-      </form>
+      <NavbarAdmin />
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <form onSubmit={handleSubmit} className="row g-3">
+              <h1>Nueva Propiedad</h1>
+              <input
+                value={province}
+                onChange={handleProvince}
+                placeholder="province"
+                className="form-control"
+              ></input>
+
+              <input
+                value={city}
+                onChange={handleCity}
+                placeholder="city"
+                className="form-control"
+              ></input>
+
+              <input
+                value={address}
+                onChange={handleAddress}
+                placeholder="address"
+                className="form-control"
+              ></input>
+
+              <input
+                value={number}
+                onChange={handleNumber}
+                placeholder="number"
+                className="form-control"
+              ></input>
+
+              <input
+                value={onSale}
+                onChange={handleOnsale}
+                placeholder="onSale"
+                className="form-control"
+              ></input>
+
+              <input
+                value={price}
+                onChange={handlePrice}
+                placeholder="price"
+                className="form-control"
+              ></input>
+
+              <input
+                value={img}
+                onChange={handleImage}
+                placeholder="img"
+                className="form-control"
+              ></input>
+
+              <input
+                value={squareMeters}
+                onChange={handleSquareMeters}
+                placeholder="squareMeters"
+                className="form-control"
+              ></input>
+
+              <input
+                value={bathrooms}
+                onChange={handleBathrooms}
+                placeholder="bathrooms"
+                className="form-control"
+              ></input>
+              <input
+                value={bedrooms}
+                onChange={handleBedrooms}
+                placeholder="bedrooms"
+                className="form-control"
+              ></input>
+              <br />
+
+              <div className="form-check-admin">
+                <div className="form-check-option">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    value="alquiler"
+                    onChange={handleOnsale}
+                    placeholder="Alquiler"
+                    id="alquiler"
+                    checked={onSale === "alquiler"}
+                  />
+                  <label className="form-check-label" htmlFor="alquiler">
+                    Alquiler
+                  </label>
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    value="comprar"
+                    onChange={handleOnsale}
+                    placeholder="comprar"
+                    id="comprar"
+                    checked={onSale === "comprar"}
+                  />
+                  <label className="form-check-label" htmlFor="comprar">
+                    comprar
+                  </label>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-success mx-2">
+                Registrar Nueva Propiedad
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
