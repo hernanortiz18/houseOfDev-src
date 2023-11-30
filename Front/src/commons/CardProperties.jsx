56b1c67885149981057802e46aeca3f8247153d9
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-30 11:20:31 -0300

    add delete button in adminProperty

--- Front/src/commons/CardProperties.jsx
+++ Front/src/commons/CardProperties.jsx
@@ -8,7 +8,7 @@ const CardProperties = ({ property }) => {
       <div className="row row-cols-2 row-cols-md-2 g-4">
         {property.length &&
           property.map((data, i) => (
-            <div className="col-2" key={i}>
+            <div className="col" key={i}>
               <UserProperty data={data} />
             </div>
           ))}
