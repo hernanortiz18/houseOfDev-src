1d26053144ffd54ddc888a92932b0a3124b12793
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-12-03 18:27:58 -0300

    change CardUserProperty

--- Front/src/commons/CardUserProperty.jsx
+++ Front/src/commons/CardUserProperty.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { Button } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/properties.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "../styles/properties.scss";
 
 function CardUserProperty({ data }) {
   const user = useSelector((state) => state.user);
@@ -34,74 +33,59 @@ function CardUserProperty({ data }) {
   };
 
   return (
-    <>
-      <div className="contenedorUserPropiedades mb-4">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="imagen">
-              <img src={data?.img} alt="Property" className="imagenPropiedad" />
+    <div className="card mb-3" style={{ maxWidth: "540px" }}>
+      <div className="row g-0">
+        <div className="col-md-5">
+          <img
+            src={data?.img}
+            className="img-fluid rounded-start"
+            style={{ width: "100%", height: "100%" }}
+            alt="..."
+          />
+        </div>
+        <div className="col-md-7">
+          <div className="card-body">
+            <div className="d-flex justify-content-between">
+              <p className="card-text">
+                <i class="fa-sharp fa-solid fa-dollar-sign"></i> {data.price}{" "}
+              </p>
+              <p className="card-text">
+                <i className="bi bi-geo-alt-fill"></i> {data.province}
+              </p>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="info">
-              <div className="div-uno">
-                <div className="div-info-uno">
-                  <div className="primerCuadrado">
-                    <p className="titulos">$ </p>
-                    <p className="margen">{data?.price}</p>
-                    <div className="lugar">
-                      <i className="bi bi-geo-alt-fill"></i>
-                      <p className="parrafoLugar">{data?.province}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className="d-flex justify-content-between">
+              <p className="card-text">
+                <i className="fa-solid fa-ruler-combined"></i>{" "}
+                {data?.squareMeters}
+                {" Mt²"}
+              </p>
+              <p className="card-text">
+                <i class="fa-solid fa-bed"></i> {data?.bedrooms} {" dorm"}
+              </p>
+              <p className="card-text">
+                <i className="fa-solid fa-bath"></i> {data?.bathrooms}
+                {" Baños"}
+              </p>
+            </div>
 
-              <div className="div-dos">
-                <div className="div-info">
-                  <i className="fa-solid fa-ruler-combined"></i>
-                  <p className="datas">{data?.squareMeters}</p>
-                  <p className="titulos">M </p>
-                </div>
-                <div className="div-info">
-                  <i class="fa-solid fa-bed"></i>
-                  <p className="datas">{data?.bedrooms}</p>
-                  <p className="titulos">Habitaciones </p>
-                </div>
-                <div className="div-info">
-                  <i className="fa-solid fa-bath"></i>
-                  <p className="datas">{data?.bathrooms}</p>
-                  <p className="titulos">Baños</p>
-                </div>
-              </div>
-
-              <div className="div-tres">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Inventore omnis deserunt beatae, sequi suscipit modi nostrum
-                  cum delectus,
-                </p>
-              </div>
-
-              <ToastContainer />
-              <div className="div-cuatro">
-                <button onClick={handleClick} variant="outline-primary">
-                  <i className="bi bi-heart-fill"></i>
+            <ToastContainer />
+            <div className="card-text">
+              <button onClick={handleClick} className="btn btn-outline-primary">
+                <i className="bi bi-heart-fill"></i>
+              </button>
+              <Link to={"contactate"}>
+                <button className="btn btn-outline-primary">
+                  <i className="bi bi-telephone-fill"></i>
                 </button>
-                <Link to={"contactate"}>
-                  <button variant="outline-primary">
-                    <i className="bi bi-telephone-fill"></i>
-                  </button>
-                </Link>
-                <Link to={`/propiedades/${data.id}`}>
-                  <button variant="outline-primary ">VER MÁS</button>
-                </Link>
-              </div>
+              </Link>
+              <Link to={`/propiedades/${data.id}`}>
+                <button className="btn btn-outline-primary">VER MÁS</button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
