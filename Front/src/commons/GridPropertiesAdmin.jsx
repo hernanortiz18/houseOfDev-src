e45019160428fd264cbf9858fd09c5bbb3775c88
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-12-04 01:57:16 -0300

    change CardAdminProperty

--- Front/src/commons/GridPropertiesAdmin.jsx
+++ Front/src/commons/GridPropertiesAdmin.jsx
@@ -6,12 +6,13 @@ import CardAdminProperty from "./CardAdminProperty";
 //GRID
 const GridPropertiesAdmin = ({ property }) => {
   return (
-    <div className="containerGrilla">
+    <div className="">
+      <h1>Todas las Propiedades</h1>
       <div className="row row-cols-2 row-cols-md-2 g-4">
         {property.length &&
           property.map((data, i) => (
-            <div className="col" key={i}>
-              <div className="">
+            <div className="col-md-3 mb-3" key={i}>
+              <div className="d-flex justify-content-center align-items-center">
                 <Link to={`propiedades/${data.id}`}>
                   <CardAdminProperty data={data} />
                 </Link>
