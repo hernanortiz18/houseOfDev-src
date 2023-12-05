daacd6a4c42a385b7f7e3d5aecc2f3a7296e9ddd
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-12-05 18:44:50 -0300

    change UserIdentification

--- Front/src/components/UserIdentification.jsx
+++ Front/src/components/UserIdentification.jsx
@@ -3,6 +3,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import NavbarAdmin from "../commons/NavbarAdmin";
 
 function UserIdentification() {
   const { id } = useParams();
@@ -17,16 +18,29 @@ function UserIdentification() {
 
   return (
     <>
-      <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">{user.name}</h5>
+      <NavbarAdmin />
+      <h1>Datos Personales</h1>
+      <div className="user-icon-container imagenIcono">
+        <img
+          src={
+            "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
+          }
+          class="card-img-top"
+          alt="..."
+        ></img>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">{user.name}</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Nombre: {user.name} </li>
+            <li className="list-group-item">Apelido: {user.lastName} </li>
+            <li className="list-group-item">E-mail: {user.email}</li>
+            <li className="list-group-item">Telefono: {user.phone}</li>
+          </ul>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Nombre: {user.name} </li>
-          <li className="list-group-item">Apelido: {user.lastName} </li>
-          <li className="list-group-item">E-mail: {user.email}</li>
-          <li className="list-group-item">Telefono: {user.phone}</li>
-        </ul>
       </div>
     </>
   );
